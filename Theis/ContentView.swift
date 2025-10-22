fd16f050dbc9d79e3df8f874327add7c9891b3ad
KxxxxN <65.285kansinee@gmail.com>
2025-10-22 15:27:16 +0700

    Initial Commit

---
+++ Theis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Theis
+//
+//  Created by Kansinee Klinkhachon on 22/10/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Theis/TheisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheisApp.swift
+//  Theis
+//
+//  Created by Kansinee Klinkhachon on 22/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TheisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
