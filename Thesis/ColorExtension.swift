a1af100781da0f8df56430a6be31c50c6a86c112
KxxxxN <65.285kansinee@gmail.com>
2025-10-22 18:15:12 +0700

    Rename project to Thesis

---
+++ Thesis/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  Theis
+//
+//  Created by Kansinee Klinkhachon on 22/10/2568 BE.
+//
+
+import SwiftUI
+
+extension Color {
+    static let mainColor = Color(red: 0x3B/255, green: 0x51/255, blue: 0x31/255)
+//    static let accentColor = Color(red: 0xF2/255, green: 0xB6/255, blue: 0x2F/255) 
+}
