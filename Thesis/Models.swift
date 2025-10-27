a98b88e83c20562b50608307f97b9117bdd788ba
Penpitcha <pinkpai1231@gmail.com>
2025-10-27 14:38:12 +0700

    HomeView

---
+++ Thesis/Models.swift
@@ -0,0 +1,24 @@
+// Models.swift
+import Foundation
+
+struct HistoryItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let date: String
+    let points: String
+    let pointsLabel: String
+}
+
+struct RecyclableItem: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+    let count: String
+}
+
+struct NavigationItem: Identifiable {
+    let id = UUID()
+    let icon: String
+    let label: String
+    var isActive: Bool
+}
