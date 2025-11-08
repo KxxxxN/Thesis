60b1d8f4ebe69ed818212dd39379e5054d0564dc
KxxxxN <65.285kansinee@gmail.com>
2025-11-08 15:34:31 +0700

    UI loginView

---
+++ Thesis/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Thesis
+//
+//  Created by Kansinee Klinkhachon on 8/11/2568 BE.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
