2d0ad009b99f997f680c6cb318bd8c5b78a0079d
Penpitcha <pinkpai1231@gmail.com>
2025-12-08 21:09:37 +0700

    DetailView, DetailWasteTypeView, FrequentWasteView, RewardExchangeView,ScoreHistoryView, WasteTypeView Edit MainApp,MainTab,HistorySection,KnowledgeView, RewardExchangeSection

---
+++ Thesis/DetailWasteTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailWasteTypeView.swift
+//  Thesis
+//
+//  Created by Penpitcha Sureepitak on 8/12/2568 BE.
+//
+
+import SwiftUI
+
+struct DetailWasteTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailWasteTypeView()
+}
