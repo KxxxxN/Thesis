10d6a5e0c4121eab32a7f975c55d7c4b35e870d1
Penpitcha <pinkpai1231@gmail.com>
2025-12-26 06:37:47 +0700

    เหลือส่วน Animation และ เชื่อมหน้า

---
+++ Thesis/BarcodeScanView.swift
@@ -0,0 +1,18 @@
+//
+//  BarcodeScanView.swift
+//  Thesis
+//
+//  Created by Penpitcha Sureepitak on 26/12/2568 BE.
+//
+
+import SwiftUI
+
+struct BarcodeScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarcodeScanView()
+}
---
+++ Thesis/QRScanView.swift
@@ -0,0 +1,18 @@
+//
+//  QRScanView.swift
+//  Thesis
+//
+//  Created by Penpitcha Sureepitak on 26/12/2568 BE.
+//
+
+import SwiftUI
+
+struct QRScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRScanView()
+}
