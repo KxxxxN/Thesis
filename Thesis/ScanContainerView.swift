5dca5aa764139f73a232e96039a8581f87a8fec4
Penpitcha <pinkpai1231@gmail.com>
2026-01-12 13:18:39 +0700

    แก้หน้าความรู้ทั่วไป และการเปลี่ยนหน้าจากหน้า QRScanView กลับไปหน้า Main

---
+++ Thesis/ScanContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanContainerView.swift
+//  Thesis
+//
+//  Created by Penpitcha Sureepitak on 12/1/2569 BE.
+//
+
+import SwiftUI
+
+struct ScanContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanContainerView()
+}
