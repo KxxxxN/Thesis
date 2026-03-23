a3a383a92d8bdd99d0ecb75965f3e488f3889147
Penpitcha <pinkpai1231@gmail.com>
2026-03-23 15:16:57 +0700

    แก้ Banner ใส่ Logo

---
+++ Thesis/FrequentWasteView (Begin).swift
@@ -0,0 +1,18 @@
+//
+//  FrequentWasteView (Begin).swift
+//  Thesis
+//
+//  Created by Penpitcha Sureepitak on 23/3/2569 BE.
+//
+
+import SwiftUI
+
+struct FrequentWasteView__Begin_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrequentWasteView__Begin_()
+}
---
+++ Thesis/ScoreHistoryView (Begin).swift
@@ -0,0 +1,18 @@
+//
+//  ScoreHistoryView (Begin).swift
+//  Thesis
+//
+//  Created by Penpitcha Sureepitak on 23/3/2569 BE.
+//
+
+import SwiftUI
+
+struct ScoreHistoryView__Begin_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreHistoryView__Begin_()
+}
